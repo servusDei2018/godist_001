27e679cb3171cd2eadb96609723174541093eb8e
Nate <37554478+servusDei2018@users.noreply.github.com>
2020-10-01 18:50:41 -0400

    Add files via upload

---
+++ src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	println("I CAN'T BELIEVE IT")
+	time.Sleep(1*time.Second)
+	println("aishiahwiehwpeahiapweh")
+	time.Sleep(1*time.Second)
+	println("You actually trusted me.")
+	time.Sleep(1*time.Second)
+	println("Well I appreciate it. Thank you <3")
+	println("Don't worry I didn't do anything.")
+	time.Sleep(5*time.Second)
+	return
+}
